2fd14bd5ea92a8d3561713e14e294dcac4d6acb5
Tim Oliver Walter <timwalter94@web.de>
2021-08-25 18:51:28 +0200

    Added Datatable to App

--- server.R
+++ server.R
@@ -2,61 +2,52 @@ library(shiny)
 library(plotly)
 library(prophet)
 library(dplyr)
+library(DT)
 
 shinyServer(function(input, output) {
-    output$forecastPlot <- renderPlotly({
-        forecast <- get_forecast()
+    forecast <- reactive({
+        forecast <- get_forecast(pred_date = input$pred_date,
+                                 end_date = input$end_date)
         
-        fig <- plot_ly(
-            forecast,
-            x = ~ ds,
-            y = ~ y,
-            name = "Price",
-            type = "scatter",
-            mode = "lines",
-            line = list(color = "rgba(49,130,189,1)")
-        )
         
-        fig <- fig %>%
-            add_trace(
-                y = ~ yhat,
-                name = "Forecast",
-                line = list(color = "rgba(230,0,0,1)")
-            )
-        
-        fig <- fig %>%
-            layout(
-                title = "Close Price",
-                xaxis = list(type = "date", title = "Date"),
-                yaxis = list(type = "linear", title = "Close Price")
-            )
-        
+    })
+    
+    output$plot <- renderPlotly({
+        fore <- forecast()
+        get_figure(forecast = fore)
+    })
+    
+    output$table <- DT::renderDataTable({
+        forecast()
     })
     
 })
 
 import_data <- function() {
-    path <- "data\\Alcohol_Sales.csv"
+    path <- "data\\alcohol_sales.csv"
     
     df <- read.csv(path,
                    na.strings = c("null", ".", ""))
-    df <- df %>% rename(ds = Date,
-                        y = Sale) %>%
+    df <- df %>%
+        rename(ds = Date,
+               y = Sale) %>%
         mutate(ds = as.Date(ds,
                             format = "%Y-%m-%d"))
     
     return(df)
 }
 
-get_forecast <- function() {
+get_forecast <- function(pred_date, end_date) {
     df <- import_data()
     
-    split_date <- "2017-12-01"
-    train <- df[df$ds <= split_date,]
-    test <- df[df$ds > split_date,]
+    ds <- seq.Date(from = as.Date("2019-02-01"),
+                   to = pred_date,
+                   by = "month")
+    
+    future_dataframe <- data.frame(ds)
     
     m <- prophet(
-        train,
+        df,
         yearly.seasonality = TRUE,
         weekly.seasonality = FALSE,
         daily.seasonality = FALSE,
@@ -67,7 +58,7 @@ get_forecast <- function() {
         changepoint.range = 0.8
     )
     
-    pred <- predict(m, test)
+    pred <- predict(m, future_dataframe)
     pred <- pred %>%
         mutate(ds = as.Date(ds,
                             format = "%Y-%m-%d")) %>%
@@ -80,5 +71,54 @@ get_forecast <- function() {
         full_join(pred,
                   by = "ds")
     
+    forecast <- forecast[forecast$ds >= end_date, ]
+    
     return(forecast)
+}
+
+get_figure <- function(forecast) {
+    fig <- plot_ly(
+        forecast,
+        x = ~ ds,
+        y = ~ y,
+        name = "Price",
+        type = "scatter",
+        mode = "lines",
+        line = list(color = "rgba(49,130,189,1)")
+    )
+    
+    fig <- fig %>%
+        add_trace(
+            y = ~ yhat,
+            name = "Forecast",
+            line = list(color = "rgba(230,0,0,1)")
+        )
+    
+    fig <- fig %>%
+        add_trace(
+            y = ~ yhat_upper,
+            line = list(color = "rgba(230,0,0,0)"),
+            showlegend = FALSE
+        )
+    
+    fig <- fig %>%
+        add_trace(
+            y = ~ yhat_lower,
+            fill = "tonexty",
+            fillcolor = "rgba(230,0,0,0.4)",
+            line = list(color = "rgba(230,0,0,0)"),
+            name = "Confidence interval"
+        )
+    
+    fig <- fig %>%
+        layout(
+            #title = "Alcohol sales forecast",
+            xaxis = list(type = "date",
+                         title = "Date"),
+            yaxis = list(type = "linear",
+                         title = "Sale"),
+            legend = list(x = 0.01, y = 1.01)
+        )
+    
+    return(fig)
 }
--- ui.R
+++ ui.R
@@ -1,7 +1,34 @@
 library(shiny)
+library(shinythemes)
 library(plotly)
+library(DT)
 
 shinyUI(fluidPage(
+  theme = shinytheme("superhero"),
   titlePanel("Forecasting Alcohol Sales"),
-  mainPanel(plotlyOutput("forecastPlot"))
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(
+        "pred_date",
+        "Prediction period:",
+        min = as.Date("2019-02-01", "%Y-%m-%d"),
+        max = as.Date("2020-02-01", "%Y-%m-%d"),
+        value = as.Date("2019-08-01"),
+        timeFormat = "%Y-%m-%d"
+      ),
+      sliderInput(
+        "end_date",
+        "Observation period:",
+        min = as.Date("1992-01-01", "%Y-%m-%d"),
+        max = as.Date("2018-11-01", "%Y-%m-%d"),
+        value = as.Date("1992-01-01"),
+        timeFormat = "%Y-%m-%d"
+      )
+    ),
+    mainPanel(
+      plotlyOutput("plot"),
+      DT::dataTableOutput("table")
+      )
+  )
 ))
+
